e76aea7b22c34a6728450c2d439403175c788ec2
YogeshN <yogirajnimbalkar@gmail.com>
2024-07-03 15:21:39 +0530

    Example3

---
+++ src/basic/PredefinedActions.java
@@ -0,0 +1,23 @@
+package basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PredefinedActions {
+
+	static WebDriver driver;
+	
+	public static WebDriver start(String url) {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		System.out.println("STEP : Maximize the browser and load url");
+		driver.manage().window().maximize();
+		driver.get(url);
+		return driver;
+	}
+	
+	public static void tearDown() {
+		System.out.println("STEP : Close the browser");
+		driver.quit();
+	}
+}
